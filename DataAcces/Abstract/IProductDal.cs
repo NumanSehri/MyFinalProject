3e09fbb59bf7eede2a9fd7820308ecc519a1d968
NumanSehri <numansehri@gmail.com>
2023-02-27 23:03:05 +0300

    Add project files.

---
+++ DataAcces/Abstract/IProductDal.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAcces.Abstract
+{
+
+    //Product ile yapacağım işlemler burda imzalanır
+    public interface IProductDal
+    {
+        //Product verileri listele
+        List<Product> GetAll();
+        //Veri Ekle
+        void Add(Product product);
+        //Veri Güncelle
+        void Update(Product product);
+        //Veri Sil
+        void Delete(Product product);
+
+
+    }
+}
---
+++ DataAcces/Concrete/InMemory/InMemoryProdutDal.cs
@@ -0,0 +1,32 @@
+﻿using DataAcces.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAcces.Concrete.InMemory
+{
+    public class InMemoryProdutDal : IProductDal
+
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
